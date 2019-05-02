16b8c5f5cd3ac4eb062ededb28f29f1c9b2f17df
Vasilii <nitraatss@gmail.com>
2019-05-02 17:18:59 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Components
-import {App} from "./components/App/index.jsx";
+import {App} from "./components/App/app.jsx";
 
 const init = () => {
   ReactDOM.render(<App />, document.querySelector(`#root`));
